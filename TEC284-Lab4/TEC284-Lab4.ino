65a3d00b46057778e478336d14c1663cb4eb8dc5
koblume <koblume@ilstu.edu>
2026-01-28 15:17:37 -0600

    Printed temperature/humidity on display
    Utilized the sensors to display the current temperature and room humidity on the OLED screen. Read the value of the temperature and humidity from the sensor using dht.readHumidity() and dht.readTemperature(). Added a calculation to convert the temperature from Celsius to Fahrenheit.

--- TEC284-Lab4/TEC284-Lab4.ino
+++ TEC284-Lab4/TEC284-Lab4.ino
@@ -4,16 +4,30 @@
 #include <Grove_Temperature_And_Humidity_Sensor.h>
 
 U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
-
+DHT dht(3, DHT11);
 void setup() {
   u8x8.begin();
   u8x8.setFlipMode(1);
   u8x8.clear();
+  
+  dht.begin();
 }
 
 void loop() {
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8.setCursor(0, 0);
-  u8x8.print("Hello World!");
+  //u8x8.print("Hello World!");
+
+  double temperature = 0;
+  double humidity = 0;
+  temperature = dht.readTemperature();
+  temperature = (temperature * 9/5) + 32;
+  
+  humidity = dht.readHumidity();
+
+  u8x8.println("Temperature: ");
+  u8x8.println(temperature);
+  u8x8.println("Humidity: ");
+  u8x8.println(humidity);
 }
 
